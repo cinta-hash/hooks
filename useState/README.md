b23958edef9771be031db449f512c9fec18d5ca9
jacinta mwangi <jacintawmwangi21@gmail.com>
2024-01-02 13:47:13 +0300

    updated readMe and code

--- useState/README.md
+++ useState/README.md
@@ -13,4 +13,8 @@ example
 setCounter(8)=> the value will be 8(fixed value)
 setCounter(counter + 1)=> the value of counter will increase by 1 every time the button is clicked. this causes react to rerender every time a value changes to show the new value.
 
-for input values, you want to update each change at every key stroke. you can use the onchange event listener
+for input values, you want to update each change at every key stroke. you can use the onchange property
+the onchange should call a function inside of it at every single key stroke and cause a rerender
+the onchange property should have the event type.
+the new value being typed should be accessible using the event.target.value
+then set the setInputValue to the newValue. REMEMBER to add the name of the function that updates the ne value, passed into onChange.
--- useState/src/App.jsx
+++ useState/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react"
 function App() {
   const [counter, setCounter]=useState(0)
-  const [inputValue, setInputVlaue]= useState("Jacinta")
+  const [inputValue, setInputValue]= useState("Jacinta")
 
   const increment = () =>{
     setCounter(counter + 1)
   }
   const decrement = () =>{
     setCounter(counter - 1)
+  }
+  let onType = (e)=>{
+    const newValue = e.target.value
+    setInputValue(newValue)
   }
  return(
   <div className="text-center text-3xl">
@@ -17,7 +21,7 @@ function App() {
     <button className="bg-red-300 p-2 rounded-md" onClick={increment}> Increment</button>
     <hr />
     <div className="mt-6">
-    <input className="indent-2 rounded-md bg-red-300" type="text" placeholder="place text here" />
+    <input className="indent-2 rounded-md bg-red-200" type="text" placeholder="place text here" onChange={onType} />
     {inputValue}
     </div>
     <input type="text" />
