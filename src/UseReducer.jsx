b1dd7f60a3fe65e2be0198d763e881f84a4bd243
jacinta mwangi <jacintawmwangi21@gmail.com>
2024-01-04 12:46:35 +0300

    added the dispatch action

--- src/UseReducer.jsx
+++ src/UseReducer.jsx
@@ -1,28 +1,27 @@
 import { useReducer } from "react"
 
 export default function UseReducer(){
-
-    const reducer=(state, action)=>{
-        switch(action.type){
-            case "INCREMENT":
-                return{count: state.count + 1, showText: state.showText}
-            case "toggleShowText":
-                return{count: state.count, showText: !state.showText}
-            default:
-                return state;
-        }
+const reducer=(state, action)=>{
+    switch(action.type){
+        case "INCREMENT":
+                return{count: state.count +1, showText: state.showText}
+        case "toggleShowText":
+            return{count: state.count, showText: !state.showText}
+        default:
+            return state
     }
+}
 
-    const [state, dispatch]=useReducer(reducer,{count: 0, showText: true} )
+const [state, dispatch]= useReducer(reducer,{count: 0, showText: true})
     
     return(
         <div>
             {state.count} <br />
             <button className="rounded-md bg-red-200"
-            onClick={()=>{
-                dispatch({type: "INCREMENT"});
-                dispatch({type: "toggleShowText"});
-            }}>
+           onClick={()=>{
+                dispatch({type: "INCREMENT"})
+                dispatch({type: "toggleShowText"})
+           }}>
                 Click Me!
             </button>
             {state.showText && <p>this text disappears</p>}
